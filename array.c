8691b16171053706886c326fd31dea4aa3216c96
Micke2310 <maikyarachan@gmail.com>
2024-04-17 16:52:46 +0000

    ...

---
+++ array.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(void)
+{
+int usuario[n] = [10,50,12,16,2];
+int max = usuario[0];
+int min = usuario[0];
+int i = 0;
+
+while (i >= 1 && i < 5)
+{
+	if (usuario[i] >= max)
+	{	
+		max = usuario;
+		i++;
+		printf("El valor maximo es : %d", max);
+	}
+
+	else
+		printf("El valor MAXIMO del usuario es: %d",max);
+}
+
+
+
+	else if (usuario < min)
+	{
+		min = usuario;
+		i++
+	}		
+	else
+		
+		
+		
+		/*printf("El valor MAXIMO del usuario es: %d",max);*/
+
+i++
+	
+}
+
+
+
+
+
+
+
+return (0);
+}
