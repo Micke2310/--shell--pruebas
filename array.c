b118cea32b27743ffa093e5362ef6998f2bcf961
Micke2310 <maikyarachan@gmail.com>
2024-04-17 17:28:22 +0000

    ...

--- array.c
+++ array.c
@@ -5,45 +5,30 @@
 
 int main(void)
 {
-int usuario[n] = [10,50,12,16,2];
-int max = usuario[0];
-int min = usuario[0];
-int i = 0;
+int usuario[] = {10,50,12,16,2};			/*declaro e inicializo array*/
+int tamaño = sizeof(usuario) / sizeof(usuario[0]);	/*calculo tamaño del array (tamaño total en bytes / tamaño del primer elemnto en bytes) */
+							  /*(5 elementos x 4 bytes = 20 bytes, dividido 1 elemnto de 4 bites == 5 elemnetos.)*/
+
+int max = usuario[0]; /*tomamos el primer elemento, indice 0 como referencia para comparacion*/
+int min = usuario[0]; /*tomamos el primer elemento, indice 0 como referencia para comparacion*/
+int i = 1;
 
 while (i >= 1 && i < 5)
 {
 	if (usuario[i] >= max)
 	{	
-		max = usuario;
-		i++;
-		printf("El valor maximo es : %d", max);
+		max = usuario[i];
 	}
 
-	else
-		printf("El valor MAXIMO del usuario es: %d",max);
-}
-
-
-
-	else if (usuario < min)
+	if (usuario[i] <= min)
 	{
-		min = usuario;
-		i++
-	}		
-	else
-		
-		
-		
-		/*printf("El valor MAXIMO del usuario es: %d",max);*/
-
-i++
+		min = usuario[i];
+	}
 	
+	i++;
 }
 
-
-
-
-
+printf("El valor MAXIMO del usuario es: %d y el MINIMO es : %d", max, min);
 
 
 return (0);
